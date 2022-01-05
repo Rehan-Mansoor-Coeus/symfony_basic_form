3a387b811b2427c2c4b20bc4c623171b2515022c
Rehan-Mansoor-Coeus <rehan.mansooor@coeus-solutions.de>
2022-01-05 18:58:44 +0500

    intrect with db using doctrine

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -25,13 +25,49 @@ class FormController extends AbstractController
         $form->handleRequest($request);
         if($form->isSubmitted()){
           $em = $this->getDoctrine()->getManager();
-
           $em->persist($post);
           $em->flush();
         }
 
         return $this->render('form/index.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/store", name="store")
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->setTitle('title from controller');
+        $post->setDescription('description from controller');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+
+
+        $form = $this->createForm(PostTYpe::class , $post , [
+            'action' => $this->generateUrl('form')
+        ]);
+
+        $form->handleRequest($request);
+
+        return $this->render('form/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/retrive", name="retrive")
+     */
+    public function retrive(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $retrive = $em->getRepository(Post::class)->findAll();
+
+        return $this->render('form/data.html.twig', [
+            'data' => $retrive
         ]);
     }
 }
